61e96507a358046091bb97d00a11b107e7d31ea0
Elton Xavier <elton.xavier@outlook.com>
2024-09-03 18:40:10 -0300

    restante da aplicação da API

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,12 +1,14 @@
 package med.voll.api.controller;
 
 
-
-import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("pacientes")
