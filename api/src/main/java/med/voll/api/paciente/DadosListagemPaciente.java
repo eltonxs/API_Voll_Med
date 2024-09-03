be58d28ff644b55237b3450970baa31ad1f52b77
Elton Xavier <elton.xavier@outlook.com>
2024-09-02 22:54:30 -0300

    first commit

---
+++ api/src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
@@ -0,0 +1,15 @@
+package med.voll.api.paciente;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import med.voll.api.endereco.DadosEndereco;
+
+
+public record DadosListagemPaciente(String nome, String email, String cpf) {
+    public DadosListagemPaciente(Paciente paciente) {
+        this(paciente.getNome(), paciente.getEmail(), paciente.getCpf());
+    }
+}
